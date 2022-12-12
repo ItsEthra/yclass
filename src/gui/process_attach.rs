ccf7382611c87c2aafd22f772fa7821872867c60
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-12-12 18:43:29 +0300

    Improved performance on process selector

--- src/gui/process_attach.rs
+++ src/gui/process_attach.rs
@@ -3,27 +3,29 @@ use eframe::{
     egui::{Context, RichText, ScrollArea, TextEdit, Window},
     epaint::{vec2, FontId},
 };
-use memflex::external::ProcessIterator;
+use memflex::external::{ProcessEntry, ProcessIterator};
 
 pub struct ProcessAttachWindow {
     shown: bool,
-    poisoned: bool,
     filter: String,
-    state: StateRef,
+    processes: Vec<ProcessEntry>,
 }
 
 impl ProcessAttachWindow {
-    pub fn new(state: StateRef) -> Self {
+    pub fn new(_: StateRef) -> Self {
         Self {
-            poisoned: false,
+            processes: vec![],
             shown: false,
             filter: "".to_owned(),
-            state,
         }
     }
 
     pub fn toggle(&mut self) {
         self.shown = !self.shown;
+
+        if self.shown {
+            self.processes = collect_processes();
+        }
     }
 
     pub fn show(&mut self, ctx: &Context) -> Option<u32> {
@@ -38,41 +40,43 @@ impl ProcessAttachWindow {
             .default_size(vec2(180., 320.))
             .show(ctx, |ui| {
                 ui.vertical_centered_justified(|ui| {
-                    TextEdit::singleline(&mut self.filter)
+                    let r = TextEdit::singleline(&mut self.filter)
                         .desired_width(f32::INFINITY)
                         .hint_text("Filter by name")
-                        .show(ui);
+                        .show(ui)
+                        .response;
 
-                    ScrollArea::vertical().show(ui, |ui| match ProcessIterator::new() {
-                        Ok(piter) => {
-                            for pe in piter.filter(|pe| {
-                                self.filter.is_empty()
-                                    || pe.name.to_lowercase().contains(&self.filter.to_lowercase())
-                            }) {
-                                if ui
-                                    .button(
-                                        RichText::new(format!("{} - {}", pe.name, pe.id))
-                                            .font(FontId::proportional(16.)),
-                                    )
-                                    .clicked()
-                                {
-                                    attach_pid = Some(pe.id);
-                                }
+                    if ui.button("Refresh").clicked() || r.changed() {
+                        self.processes = collect_processes();
+                    }
+
+                    ui.add_space(4.);
+                    ui.separator();
+                    ui.add_space(4.);
+
+                    ScrollArea::vertical().show(ui, |ui| {
+                        for pe in self.processes.iter().filter(|pe| {
+                            self.filter.is_empty()
+                                || pe.name.to_lowercase().contains(&self.filter.to_lowercase())
+                        }) {
+                            if ui
+                                .button(
+                                    RichText::new(format!("{} - {}", pe.name, pe.id))
+                                        .font(FontId::proportional(16.)),
+                                )
+                                .clicked()
+                            {
+                                attach_pid = Some(pe.id);
                             }
                         }
-                        Err(e) if self.poisoned => {
-                            _ = self
-                                .state
-                                .borrow_mut()
-                                .toasts
-                                .error(format!("Failed to iterate over processes: {e}"));
-                            self.poisoned = true;
-                        }
-                        _ => {}
                     });
                 });
             });
 
         attach_pid
     }
+}
+
+fn collect_processes() -> Vec<ProcessEntry> {
+    ProcessIterator::new().into_iter().flatten().collect()
 }
