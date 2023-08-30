8c65d333ac9eea8624d8e77d4d223f456018a52e
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-08-30 11:53:22 +0300

    Removed default code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import "./styles.css";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+    target: document.getElementById("app")!,
 });
 
 export default app;
